04442e0b0c475cc8a15671fbe31fdb7aef4831a6
eyalraifler <eyalraifler3@gmail.com>
2026-01-01 17:56:48 +0200

    add all 3 protein files

---
+++ scripts/main.py
@@ -0,0 +1,14 @@
+import sys
+
+def ask_user_for_protein():
+    user_input = 0
+    while user_input != 1 and user_input != 2 and user_input != 3:
+        user_input = int(input("Please enter the number of a protein: 1-3W8W, 2-4BS7, 3-2YMW: "))
+    if user_input == 1:
+        file = 
+    
+
+
+
+if __name__ == "__main__":
+    ask_user_for_protein()
